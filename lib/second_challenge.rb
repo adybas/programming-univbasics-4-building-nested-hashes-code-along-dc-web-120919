c2afcfb2299a25dc8703a3066ad06c3e8f164f4f
Anna Dybas <a.m.dybas@gmail.com>
2019-12-01 05:54:19 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,7 +2,10 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
-      :patriarch => {},
+      :patriarch => {
+        :name => "Lord Montague",
+        :age => "53"
+      },
       :matriarch => {},
       :hero => {},
       :hero_friends => []
